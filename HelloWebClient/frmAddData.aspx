de960565f4e1044b9b829aecf55f796957c5e560
dilanm1 <dilanm1@yahoo.co.uk>
2016-01-18 13:44:04 +0000

    changes

---
+++ HelloWebClient/frmAddData.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmAddData.aspx.cs" Inherits="HelloWebClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Pyraleads Sample Web Site</h1>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="First Name: " Width="120px"></asp:Label><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Surname: " Width="120px"></asp:Label><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Email: " Width="120px"></asp:Label><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Mobile: " Width="120px"></asp:Label><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />  
+         <asp:Label ID="Label6" runat="server" Text="Debt Level: " Width="120px"></asp:Label><asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />       
+         <asp:Label ID="Label7" runat="server" Text="Monthly Payment: " Width="120px"></asp:Label><asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <br />            
+    </div>
+        <br /> 
+        <br /> 
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Submit Data" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+
+        <br />
+        <asp:Label ID="Label8" runat="server"></asp:Label>
+
+    </div>
+        <br /> 
+        <br /> 
+    <div>
+        <h2>Web Lead Table</h2>
+        <asp:GridView ID="grvCustomers" runat="server" AllowPaging="True" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical" OnPageIndexChanging="grvCustomers_PageIndexChanging">
+            <AlternatingRowStyle BackColor="#DCDCDC" />
+            <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+            <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#0000A9" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#000065" />
+        </asp:GridView>
+    </div>
+
+    </form>
+</body>
+</html>
+
---
+++ HelloWebClient/frmAddData.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace HelloWebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+         //string connectionString = ConfigurationManager.ConnectionStrings["Genius_ConnectionString"].ConnectionString;
+         //SqlConnection conn = new SqlConnection( ConfigurationManager.ConnectionStrings["Genius_ConnectionString"].ConnectionString);
+       
+      //  string url = HttpContext.Current.Request.Url.Host;
+         string url = HttpContext.Current.Request.Url.AbsoluteUri;
+
+        
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (conn.State == ConnectionState.Closed)
+            //{
+            //    conn.Open();
+            //}
+            //if (!Page.IsPostBack)
+            //{
+            //   BindData();
+            //}
+        }
+        void BindData()
+        {
+            try
+            {
+
+
+                //  conn.Open();
+                //SqlCommand comm = new SqlCommand("select WebLeadID,Planid,AccountRef,Firstname,Surname,Email,Tel1,DebtLevel,MonthlyPayment,website from cic_HBwebleads Order By WebLeadID DESC ", conn);
+                //SqlDataAdapter adp = new SqlDataAdapter(comm);
+                //DataSet ds = new DataSet();
+                //adp.Fill(ds);
+                //grvCustomers.DataSource = ds;
+                //grvCustomers.DataBind();
+                //comm.Connection.Close();
+                //comm.Connection.Dispose();
+
+
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+            finally
+            {
+                //close the database connection
+              //  conn.Close();
+            }
+
+        }
+        void ShowMessage(string msg)
+        {
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('" + msg + "');</script>");
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HelloService.HelloServiceClient cl = new HelloService.HelloServiceClient("BasicHttpBinding_IHelloService");
+            cl.UrlIsValid(url);          
+
+            HelloService.HelloServiceClient client = new HelloService.HelloServiceClient("BasicHttpBinding_IHelloService");
+            Label1.Text = client.ProcessDataAdd(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text, TextBox6.Text, url);
+
+            Label8.Text = url;
+         //   BindData();
+         
+        }
+
+        protected void grvCustomers_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grvCustomers.PageIndex = e.NewPageIndex;
+            BindData();
+        }
+
+       
+     
+    }
+}
